3349921bcc2c4d62335bc6e64682ce3c5047cff7
Sam Stokes <me@samstokes.co.uk>
2016-01-30 01:29:11 -0800

    Use Confluent's newer librdkafka build

--- debian/pbuilder-hooks/E10-apt.ppa-stub-bottledwater.sh
+++ debian/pbuilder-hooks/E10-apt.ppa-stub-bottledwater.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Add stub/bottledwater PPA for its builds of libavro and librdkafka
+# Add stub/bottledwater PPA for its libavro package.
 # N.B. We always use the trusty PPA even when building for precise, since the
 # PPA doesn't have a precise build.  *shrug* Seems to work!
 
