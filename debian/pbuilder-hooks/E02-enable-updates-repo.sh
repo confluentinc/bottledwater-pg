cd8e0105ad20887c5f9977ead61556ca68177468
Sam Stokes <me@samstokes.co.uk>
2016-01-29 18:23:38 -0800

    Install updates repo to get fixed ssl-cert package

---
+++ debian/pbuilder-hooks/E02-enable-updates-repo.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+codename=$(lsb_release --codename --short)
+
+# Add updates repo.  This is mostly to get a fixed version of the ssl-certs
+# package, which is broken in precise on hosts with long hostnames, such as
+# our chroot-within-Travis seems to have:
+#
+# https://bugs.launchpad.net/ubuntu/+source/ssl-cert/+bug/1004682
+
+echo "deb http://archive.ubuntu.com/ubuntu/ ${codename}-updates main universe" >> /etc/apt/sources.list
