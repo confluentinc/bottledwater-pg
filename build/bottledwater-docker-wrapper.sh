64715dd1af6e9dcfb8203642c462ecf35fb1d4be
Sam Stokes <me@samstokes.co.uk>
2016-04-17 17:23:18 -0700

    Support overriding BW command line opts via env vars...
    This makes it easier to start BW in various different configurations
    using docker-compose (basically a workaround for `docker-compose up` not
    allowing you to specify command-line arguments).

--- build/bottledwater-docker-wrapper.sh
+++ build/bottledwater-docker-wrapper.sh
@@ -1,19 +1,42 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+log() { echo "$0: $@" >&2; }
+
+declare -a bw_opts
 
 POSTGRES_CONNECTION_STRING="hostaddr=$POSTGRES_PORT_5432_TCP_ADDR port=$POSTGRES_PORT_5432_TCP_PORT dbname=postgres user=postgres"
 KAFKA_BROKER="$KAFKA_PORT_9092_TCP_ADDR:$KAFKA_PORT_9092_TCP_PORT"
+bw_opts+=(--postgres="$POSTGRES_CONNECTION_STRING" --broker="$KAFKA_BROKER")
+
+for var in "${!BOTTLED_WATER_@}"; do
+  option=$(sed 's/^BOTTLED_WATER_//' <<<"$var" | tr '[:upper:]_' '[:lower:]-')
+  value=${!var}
+  case $(tr '[:upper:]' '[:lower:]' <<<"$value") in
+    "")
+      # Probably set by docker-compose env passthrough, ignore
+      ;;
+    true | yes | y | 1)
+      # boolean options don't admit arguments
+      log "Setting option --$option"
+      bw_opts+=(--"$option")
+      ;;
+    false | no | n | 0)
+      log "WARNING: Ignoring environment variable $var=$value, no support for explicitly negating option --$option"
+      ;;
+    *)
+      log "Setting option --$option=$value"
+      bw_opts+=(--"$option"="$value")
+      ;;
+  esac
+done
 
 if [ -n "$SCHEMA_REGISTRY_PORT_8081_TCP_ADDR" ]; then
   SCHEMA_REGISTRY_URL="http://${SCHEMA_REGISTRY_PORT_8081_TCP_ADDR}:${SCHEMA_REGISTRY_PORT_8081_TCP_PORT}"
 
-  schema_registry_opts="--schema-registry=$SCHEMA_REGISTRY_URL"
-else
-  schema_registry_opts=
+  log "Detected schema registry, setting --schema-registry=$SCHEMA_REGISTRY_URL"
+  bw_opts+=(--schema-registry="$SCHEMA_REGISTRY_URL")
 fi
 
-exec /usr/local/bin/bottledwater \
-    --postgres="$POSTGRES_CONNECTION_STRING" \
-    --broker="$KAFKA_BROKER" \
-    $schema_registry_opts \
-    "$@"
-
+BOTTLEDWATER=/usr/local/bin/bottledwater
+log "Running: $BOTTLEDWATER ${bw_opts[@]} $@"
+exec "$BOTTLEDWATER" "${bw_opts[@]}" "$@"
