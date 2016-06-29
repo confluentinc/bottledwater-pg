791b1192db4efd291c2c30f50392afb0e3479fad
Dat Tran Dinh Thanh <trandinhthanhdat@chotot.vn>
2016-06-29 20:55:55 +0700

    bottle-water: add snappy config file for avro installing

--- avro_install.sh
+++ avro_install.sh
@@ -5,6 +5,12 @@ echo "+---------------+"
 echo "|               |"
 echo "+---------------+"
 
+echo "Name: libsnappy" > /usr/share/pkgconfig/libsnappy.pc
+echo "Description: Snappy is a compression library" >> /usr/share/pkgconfig/libsnappy.pc
+echo "Version: 1.1.2" >> /usr/share/pkgconfig/libsnappy.pc
+echo "URL: https://google.github.io/snappy/" >> /usr/share/pkgconfig/libsnappy.pc
+echo "Libs: -L/usr/lib -lsnappy" >> /usr/share/pkgconfig/libsnappy.pc
+echo "Cflags: -I/usr/include" >> /usr/share/pkgconfig/libsnappy.pc
 echo "Installing avro 1.8.1"
 wget http://www-eu.apache.org/dist/avro/avro-1.8.1/c/avro-c-1.8.1.tar.gz
 tar -xzvf avro-c-1.8.1.tar.gz
