9fcc0ed7d327f8aaec8a684b63540e18c677fb9c
Dat Tran Dinh Thanh <trandinhthanhdat@chotot.vn>
2016-06-30 14:27:03 +0700

    bottle-water: add new package build script for bottledwater

--- package.sh
+++ package.sh
@@ -7,18 +7,15 @@ echo "|Need a more delicated script|"
 echo "+----------------------------+"
 ###
 ###
-ORG_DIR = pwd
-PACK_DIR = "bottledwater-0.1"
+ORG_DIR=$(pwd)
+PACK_DIR=bottledwater-0.1
+EXECUTABLE=bottledwater
 
-make clean && make
-cd
-mkdir ${PACK_DIR}
-mkdir ${PACK_DIR}/DEBIAN
-mkdir ${PACK_DIR}/${ORG_DIR}
+make clean && make && cd
+mkdir -p ${PACK_DIR}/DEBIAN
+mkdir -p ${PACK_DIR}${ORG_DIR}/kafka
 cp ${ORG_DIR}/control ${PACK_DIR}/DEBIAN/
-cp -avr ${ORG_DIR}/client ${PACK_DIR}/${ORG_DIR}
-cp -avr ${ORG_DIR}/ext ${PACK_DIR}/${ORG_DIR}
-cp -avr ${ORG_DIR}/kafka ${PACK_DIR}/${ORG_DIR}
+cp -avr ${ORG_DIR}/kafka/${EXECUTABLE} ${PACK_DIR}${ORG_DIR}/kafka
 dpkg --build ${PACK_DIR}
 rm -rf ${PACK_DIR}
 
