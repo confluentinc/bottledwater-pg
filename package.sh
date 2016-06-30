2bd69e024f51f92406f1ed09c39b3678215b6baa
Dat Tran Dinh Thanh <trandinhthanhdat@chotot.vn>
2016-06-30 11:48:54 +0700

    bottle-water: fix build package script

--- package.sh
+++ package.sh
@@ -10,16 +10,22 @@ echo "+----------------------------+"
 
 make clean && make
 cd ..
+rm -rf bottledwater-0.1
 mkdir bottledwater-0.1
 mkdir bottledwater-0.1/DEBIAN
 mkdir bottledwater-0.1/root
+mkdir bottledwater-0.1/root/bottledwater
 cp bottledwater/control bottledwater-0.1/DEBIAN/
-cp -avr bottledwater bottledwater-0.1/root/
+cp -avr bottledwater/client bottledwater-0.1/root/bottledwater/
+cp -avr bottledwater/ext bottledwater-0.1/root/bottledwater/
+cp -avr bottledwater/kafka bottledwater-0.1/root/bottledwater/
 dpkg --build bottlewater-0.1
 rm -rf bottlewater-0.1
 
 ###
 ###
-echo "+---------------+"
-echo "|     DONE      |"
-echo "+---------------+"
+echo "+----------------------------+"
+echo "|Temporary script            |"
+echo "|Need a more delicated script|"
+echo "|            DONE            |"
+echo "+----------------------------+"
