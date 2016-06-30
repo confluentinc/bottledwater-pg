1bfed010e1ce905493520f81fe15944995625b53
Dat Tran Dinh Thanh <trandinhthanhdat@chotot.vn>
2016-06-30 11:44:00 +0700

    bottle-water: fix package build file

--- package.sh
+++ package.sh
@@ -13,8 +13,8 @@ cd ..
 mkdir bottledwater-0.1
 mkdir bottledwater-0.1/DEBIAN
 mkdir bottledwater-0.1/root
-cp bottlewater/control bottledwater-0.1/DEBIAN/
-cp -avr bottledwater bottlewater-0.1/root/
+cp bottledwater/control bottledwater-0.1/DEBIAN/
+cp -avr bottledwater bottledwater-0.1/root/
 dpkg --build bottlewater-0.1
 rm -rf bottlewater-0.1
 
