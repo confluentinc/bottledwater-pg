46eaa34e8d924345c5e98e7a8fb800e9b497cc0d
Dat Tran Dinh Thanh <trandinhthanhdat@chotot.vn>
2016-07-07 13:54:19 +0700

    bottle-water: fix avro_build script

--- avro_install.sh
+++ avro_install.sh
@@ -12,15 +12,15 @@ echo "Name: libsnappy" > /usr/share/pkgconfig/libsnappy.pc
 echo "Description: Snappy is a compression library" >> /usr/share/pkgconfig/libsnappy.pc
 echo "Version: 1.1.2" >> /usr/local/lib/pkgconfig/libsnappy.pc
 echo "URL: https://google.github.io/snappy/" >> /usr/share/pkgconfig/libsnappy.pc
-echo "Libs: -L/usr/lib -lsnappy" >> /usr/share/pkgconfig/libsnappy.pc
-echo "Cflags: -I/usr/include" >> /usr/share/pkgconfig/libsnappy.pc
+echo "Libs: -L/usr/local/lib -lsnappy" >> /usr/share/pkgconfig/libsnappy.pc
+echo "Cflags: -I/usr/local/include" >> /usr/share/pkgconfig/libsnappy.pc
 echo "Installing avro 1.8.1"
 wget http://www-eu.apache.org/dist/avro/avro-1.8.1/c/avro-c-1.8.1.tar.gz
 tar -xzvf avro-c-1.8.1.tar.gz
 cd avro-c-1.8.1/
 mkdir build
 cd build
-cmake ..  -DCMAKE_INSTALL_PREFIX=$PREFIX        -DCMAKE_BUILD_TYPE=RelWithDebInfo
+cmake ..  -DCMAKE_INSTALL_PREFIX=$PREFIX        -DCMAKE_BUILD_TYPE=Release
 make
 make install
 cd ../../
