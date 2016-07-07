e0497618fe85e3a7e8edaccc32b818531fa4d2ef
Dat Tran Dinh Thanh <trandinhthanhdat@chotot.vn>
2016-07-07 13:39:41 +0700

    bottle-water: add new dependecies for bottle-water build script

--- avro_install.sh
+++ avro_install.sh
@@ -6,14 +6,14 @@ echo "|               |"
 echo "+---------------+"
 
 echo "Install dependencies"
-sudo apt-get install libsnappy1 cmake
+sudo apt-get install libjansson-dev cmake libsnappy1 pkg-config
 
-echo "Name: libsnappy" > /usr/local/lib/pkgconfig/libsnappy.pc
-echo "Description: Snappy is a compression library" >> /usr/local/lib/pkgconfig/libsnappy.pc
+echo "Name: libsnappy" > /usr/share/pkgconfig/libsnappy.pc
+echo "Description: Snappy is a compression library" >> /usr/share/pkgconfig/libsnappy.pc
 echo "Version: 1.1.2" >> /usr/local/lib/pkgconfig/libsnappy.pc
-echo "URL: https://google.github.io/snappy/" >> /usr/local/lib/pkgconfig/libsnappy.pc
-echo "Libs: -L/usr/lib -lsnappy" >> /usr/local/lib/pkgconfig/libsnappy.pc
-echo "Cflags: -I/usr/include" >> /usr/local/lib/pkgconfig/libsnappy.pc
+echo "URL: https://google.github.io/snappy/" >> /usr/share/pkgconfig/libsnappy.pc
+echo "Libs: -L/usr/lib -lsnappy" >> /usr/share/pkgconfig/libsnappy.pc
+echo "Cflags: -I/usr/include" >> /usr/share/pkgconfig/libsnappy.pc
 echo "Installing avro 1.8.1"
 wget http://www-eu.apache.org/dist/avro/avro-1.8.1/c/avro-c-1.8.1.tar.gz
 tar -xzvf avro-c-1.8.1.tar.gz
