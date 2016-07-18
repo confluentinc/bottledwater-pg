7249a042bf55015512b1b48a48ca6d1e92686769
root <root@stg-dbs-36.(none)>
2016-07-18 13:49:25 +0700

    bottle-water: fix rpm build script for bottle-water extension

--- RPM/BOTTLE_RPM/bottle.spec
+++ RPM/BOTTLE_RPM/bottle.spec
@@ -4,27 +4,16 @@ Version: 0.1
 Release: 1
 Group: Applications/Extension
 Packager: Dat Tran
-BuildRoot: %{_topdir}
+AutoReqProv: no
 BuildArch: noarch
 License: No License
 
 %description
 This is an extension for PostgreSQL 9.4, it will stream all wal_log to bottledwater client
 
-%prep
-
-%build
-
-%install
-
-%clean
-
 %files
 %defattr(-,root,root)
-%{_pgsqllibdir}/bottledwater.so
-%{_pgsqlsharedir}/bottledwater.control
-%{_pgsqlsharedir}/bottledwater--0.1.sql
+%(echo $(pg_config --libdir))/bottledwater.so
+%(echo $(pg_config --sharedir))/extension/bottledwater.control
+%(echo $(pg_config --sharedir))/extension/bottledwater--0.1.sql
 
-%post
-
-%postun
--- rpm_bottle_extension.sh
+++ rpm_bottle_extension.sh
@@ -7,6 +7,9 @@ echo "|Need a more delicated script|"
 echo "+----------------------------+"
 ###
 ###
+
+yum install -y zlib-devel.x86_64 snappy-devel.x86_64 gcc g++ jansson-devel.x86_64 libcurl-devel.x86_64 postgresql94-devel.x86_64
+
 ORG_DIR=$(pwd)
 PG_SHAREDIR=$(pg_config --sharedir)
 PG_LIBDIR=$(pg_config --libdir)
@@ -14,21 +17,17 @@ PACK_DIR=bottledwater-0.1
 
 make clean && make
 mkdir -p ${PACK_DIR}/RPMBUILD
-mkdir -p ${PACK_DIR}/RPMBUILD/SPECS
-mkdir -p ${PACK_DIR}/RPMBUILD/RPMS
-mkdir -p ${PACK_DIR}/RPMBUILD/SRPMS
-mkdir -p ${PACK_DIR}/RPMBUILD${PG_SHAREDIR}
+mkdir -p ${PACK_DIR}/RPMBUILD${PG_SHAREDIR}/extension
 mkdir -p ${PACK_DIR}/RPMBUILD${PG_LIBDIR}
 
-cp -avr ${ORG_DIR}/ext/bottledwater.so ${PACK_DIR}/RPMBUILD${PG_SHAREDIR}
-cp -avr ${ORG_DIR}/ext/bottledwater.control ${PACK_DIR}/RPMBUILD${PG_LIBDIR}
-cp -avr ${ORG_DIR}/ext/bottledwater--0.1.sql ${PACK_DIR}/RPMBUILD${PG_LIBDIR}
+cp -avr ${ORG_DIR}/ext/bottledwater.so ${PACK_DIR}/RPMBUILD${PG_LIBDIR}
+cp -avr ${ORG_DIR}/ext/bottledwater.control ${PACK_DIR}/RPMBUILD${PG_SHAREDIR}/extension
+cp -avr ${ORG_DIR}/ext/bottledwater--0.1.sql ${PACK_DIR}/RPMBUILD${PG_SHAREDIR}/extension
 
-cp -avr ${ORG_DIR}/RPM/.rpmmacros ~/
-
-rpmbuild -bb ${ORG_DIR}/RPM/BOTTLE_RPM/bottle.spec
+rpmbuild -bb --buildroot $(pwd)/${PACK_DIR}/RPMBUILD  ${ORG_DIR}/RPM/BOTTLE_RPM/bottle.spec
 
 rm -rf ${PACK_DIR}
+make clean
 
 ###
 ###
