fff01ddecc6465c7b1767ff8b0055127d48c2d32
Sam Stokes <me@samstokes.co.uk>
2016-08-24 17:20:19 -0700

    Tests for large values

--- spec/functional/error_handling_spec.rb
+++ spec/functional/error_handling_spec.rb
@@ -1,10 +1,13 @@
 require 'spec_helper'
+require 'format_contexts'
 require 'test_cluster'
 
-describe 'outages', functional: true do
+describe 'error handling', functional: true, format: :json do
   after(:example) do
     TEST_CLUSTER.stop(dump_logs: false)
   end
+
+  LONG_STRING = ('x' * 2_000_000).freeze
 
   let(:postgres) { TEST_CLUSTER.postgres }
 
@@ -37,6 +40,16 @@ describe 'outages', functional: true do
 
         expect(TEST_CLUSTER.bottledwater_running?).to be_falsy
       end
+    end
+
+    example 'writing a large value crashes Bottled Water' do
+      TEST_CLUSTER.start
+
+      postgres.exec('CREATE TABLE events (id SERIAL PRIMARY KEY, event TEXT)')
+      postgres.exec_params('INSERT INTO events (event) VALUES ($1)', [LONG_STRING])
+      sleep 5
+
+      expect(TEST_CLUSTER.bottledwater_running?).to be_falsy
     end
   end
 
@@ -79,6 +92,20 @@ describe 'outages', functional: true do
 
         expect(TEST_CLUSTER.bottledwater_running?).to be_truthy
       end
+    end
+
+    example 'a row with a large value gets skipped without stopping replication' do
+      TEST_CLUSTER.start
+
+      postgres.exec('CREATE TABLE events (id SERIAL PRIMARY KEY NOT NULL, event TEXT)')
+      postgres.exec_params('INSERT INTO events (event) VALUES ($1)', ['Wednesday'])
+      postgres.exec_params('INSERT INTO events (event) VALUES ($1)', [LONG_STRING])
+      postgres.exec_params('INSERT INTO events (event) VALUES ($1)', ['Friday'])
+      sleep 1
+
+      messages = kafka_take_messages('events', 2)
+      events = messages.map {|message| fetch_string(decode_value(message.value), 'event') }
+      expect(events).to eq(['Wednesday', 'Friday'])
     end
   end
 end
