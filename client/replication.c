5d874285bbaeadcac978a73ea54ca7701a31420d
Sam Stokes <me@samstokes.co.uk>
2016-08-29 15:52:05 -0700

    Quote output plugin param name for consistency

--- client/replication.c
+++ client/replication.c
@@ -145,7 +145,7 @@ int replication_stream_check(replication_stream_t stream) {
  * starting from position stream->start_lsn. */
 int replication_stream_start(replication_stream_t stream, const char *error_policy) {
     PQExpBuffer query = createPQExpBuffer();
-    appendPQExpBuffer(query, "START_REPLICATION SLOT \"%s\" LOGICAL %X/%X (error_policy '%s')",
+    appendPQExpBuffer(query, "START_REPLICATION SLOT \"%s\" LOGICAL %X/%X (\"error_policy\" '%s')",
             stream->slot_name,
             (uint32) (stream->start_lsn >> 32), (uint32) stream->start_lsn,
             error_policy);
