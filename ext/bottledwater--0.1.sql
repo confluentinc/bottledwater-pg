347f605351fa0d9d6cf05c6bd50b013ed53d807e
Dat Tran Dinh Thanh <trandinhthanhdat@chotot.vn>
2016-08-31 10:57:25 +0700

    fix error in bottledwater--0.1.sql declaration happens after resolving confict

--- ext/bottledwater--0.1.sql
+++ ext/bottledwater--0.1.sql
@@ -21,7 +21,7 @@ CREATE DOMAIN bottledwater_error_policy AS text
 CREATE OR REPLACE FUNCTION bottledwater_export(
         table_pattern text    DEFAULT '%',
         schema_pattern  text    DEFAULT '%',
-        allow_unkeyed boolean DEFAULT false
+        allow_unkeyed boolean DEFAULT false,
         error_policy bottledwater_error_policy DEFAULT 'exit'
     ) RETURNS setof bytea
     AS 'bottledwater', 'bottledwater_export' LANGUAGE C VOLATILE STRICT;
